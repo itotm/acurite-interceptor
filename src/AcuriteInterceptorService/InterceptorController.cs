1b04d466ef4f34c58e16cd262d7208ff54a59c75
itotm <ito.tm@libero.it>
2018-03-10 07:36:42 +0100

    conversion to millibars

--- src/AcuriteInterceptorService/InterceptorController.cs
+++ src/AcuriteInterceptorService/InterceptorController.cs
@@ -13,8 +13,8 @@ namespace AcuriteInterceptorService
 	{
 		public IHttpActionResult Get(string dateutc, string action, string realtime, string id, string mt, string sensor, decimal humidity, decimal tempf, decimal baromin, string battery, int rssi)
 		{
-			Console.WriteLine(String.Format("{0:dd/MM/yyyy HH:mm:ss} - Sensor: {1}, H: {2} T: {3:00.0}, P: {4}, Batt: {5}, Signal: {6}",
-				DateTime.Now, sensor, humidity, (tempf - 32) * 5 / 9, baromin, battery, rssi));
+			Console.WriteLine(String.Format("{0:dd/MM/yyyy HH:mm:ss} - Sensor: {1}, H: {2} T: {3:00.0}, P: {4:0.}, Batt: {5}, Signal: {6}",
+				DateTime.Now, sensor, humidity, (tempf - 32) * 5 / 9, baromin * 33.8637526m, battery, rssi));
 
 			if (Properties.Settings.Default.ForwardEnabled)
 			{
